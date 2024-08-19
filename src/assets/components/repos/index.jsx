577abef961996e47cd9b38d9cd374b76d8d41dd1
Khai221 <juliocnarciso@hotmail.com>
2024-08-18 22:51:23 -0400

    mais arquivos

--- src/assets/components/repos/index.jsx
+++ src/assets/components/repos/index.jsx
@@ -2,16 +2,24 @@ import { useEffect, useState } from 'react';
 
 const ReposList = () => {
     const [repos, setRepos] = useState([])
+    const [estaCarregando, setEstaCarregando] = useState(true); // esta carregando = true enquanto a página carrega e ainda nao apareceram os repositorios
 
     useEffect(() => {
         fetch('https://api.github.com/users/Khai221/repos')
         .then(response => response.json())
         .then(data => {
-            setRepos(data)
+            setTimeout(() => {
+                setEstaCarregando(false) // esta carregando = false depois de 5 segundos do settimeout para simular um carregamento
+                setRepos(data)
+            }, 5000)
         })
     }, []);
 
     return (
+        <>
+        {estaCarregando && (
+            <h1>Carregando...</h1>
+        )}
         <ul>
             {repos.map(repositorio => (
                 <li key={repositorio.id}>
@@ -22,6 +30,7 @@ const ReposList = () => {
             ))}
             <li>Repositório</li>
         </ul>
+        </>
     )
 }
 
